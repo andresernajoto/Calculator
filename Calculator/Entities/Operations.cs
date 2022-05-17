b9ddfe8e8e24a54e301bafe0a8f213b142a0654a
André Luis dos Santos Sernajoto <69870279+andresernajoto@users.noreply.github.com>
2022-05-16 21:39:56 -0300

    Updating classes
    Updating the content of classes.

--- Calculator/Entities/Operations.cs
+++ Calculator/Entities/Operations.cs
@@ -1,40 +1,54 @@
 ﻿namespace Calculator.Entities {
     class Operations {
 
-        public int FirstNum { get; set; }
-        public int SecondNum { get; set; }
+        // atributes
+        public double FirstNum { get; set; }
+        public double SecondNum { get; set; }
 
+        // construct with no parameters
         public Operations() { }
 
-        public Operations(int firstNum, int secondNum) {
+        // construct with atributes
+        public Operations(double firstNum, double secondNum) {
             FirstNum = firstNum;
             SecondNum = secondNum;
         }
 
-        public int Summing(int num1, int num2) {
+        // does the sum calcule
+        public double Summing(double num1, double num2) {
             return num1 + num2;
         }
 
-        public int Subtracting(int num1, int num2) {
+        // does the subtraction calcule
+        public double Subtracting(double num1, double num2) {
             return num1 - num2;
         }
 
-        public int Multiplying(int num1, int num2) {
+        // does the multiplication calcule
+        public double Multiplying(double num1, double num2) {
             return num1 * num2;
         }
 
-        public int Dividing(int num1, int num2) {
-            return num1 / num2;
+        // does the division calcule
+        public double Dividing(double num1, double num2) {
+            if (num1 == 0 || num2 == 0) {
+                return 0;
+            } else {
+                return num1 / num2;
+            }
+            
         }
 
-        public int RestOfDivision(int num1, int num2) {
+        // does the rest of division calcule
+        public double RestOfDivision(double num1, double num2) {
             return num1 % num2;
         }
 
-        public int Exponentiation(int num1, int num2) {
-            int tot = 1;
+        // does the exponentiation calcule
+        public double Exponentiation(double num1, double num2) {
+            double tot = 1;
 
-            for (int i = 0; i < num2; i++) {
+            for (double i = 0; i < num2; i++) {
                 tot *= num1;
             }
 
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -5,23 +5,23 @@ namespace Calculator {
     class Program {
         static void Main(string[] args) {
 
-            // calculating variables
-            int num1, num2, total;
+            // values to calculate
+            double num1, num2;
 
+            // instantiation of Operations class
             Operations op = new Operations();
 
-            // reading of calculation operator
+            // reading of the operator to be calculated
             Console.WriteLine("1 - sum\n2 - subtract\n3 - multiply\n4 - divide\n5 - rest of division\n6 - exponentiation\n0 - exit");
             Console.Write("\nChoose one of the operations above: ");
             int num = int.Parse(Console.ReadLine());
-            Console.WriteLine();
 
             // calculation structure
             while (num != 0) {
                 switch (num) {
-                    // does the sum calcule
+                    // sum calcule
                     case 1:
-                        // reading of values
+                        // reading values
                         Console.Write("Enter the first number: ");
                         num1 = int.Parse(Console.ReadLine());
                         Console.Write("Enter the second number: ");
@@ -30,19 +30,19 @@ namespace Calculator {
                         // show the result
                         Console.WriteLine("Result: " + op.Summing(num1, num2));
                         
-                        // console reseting
+                        // console reset
                         Console.Write("\nPress enter: ");
                         Console.ReadLine();
                         Console.Clear();
 
-                        // reading again the calculation operator
+                        // reading of the operator to be calculated again
                         Console.WriteLine("1 - sum\n2 - subtract\n3 - multiply\n4 - divide\n5 - rest of division\n6 - exponentiation\n0 - exit");
                         Console.Write("\nChoose one of the operations above: ");
                         num = int.Parse(Console.ReadLine());
                         break;
-                    // does the subtraction calcule
+                    // subtraction calcule
                     case 2:
-                        // reading of values
+                        // reading values
                         Console.Write("Enter the first number: ");
                         num1 = int.Parse(Console.ReadLine());
                         Console.Write("Enter the second number: ");
@@ -51,19 +51,19 @@ namespace Calculator {
                         // show the result
                         Console.WriteLine("Result: " + op.Subtracting(num1, num2));
 
-                        // console reseting
+                        // console reset
                         Console.Write("\nPress enter: ");
                         Console.ReadLine();
                         Console.Clear();
 
-                        // reading again the calculation operator
+                        // reading of the operator to be calculated again
                         Console.WriteLine("1 - sum\n2 - subtract\n3 - multiply\n4 - divide\n5 - rest of division\n6 - exponentiation\n0 - exit");
                         Console.Write("\nChoose one of the operations above: ");
                         num = int.Parse(Console.ReadLine());
                         break;
-                    // does the multiplication calcule
+                    // multiplication calcule
                     case 3:
-                        // reading of values
+                        // reading values
                         Console.Write("Enter the first number: ");
                         num1 = int.Parse(Console.ReadLine());
                         Console.Write("Enter the second number: ");
@@ -72,19 +72,19 @@ namespace Calculator {
                         // show the result
                         Console.WriteLine("Result: " + op.Multiplying(num1, num2));
 
-                        // console reseting
+                        // console reset
                         Console.Write("\nPress enter: ");
                         Console.ReadLine();
                         Console.Clear();
 
-                        // reading again the calculation operator
+                        // reading of the operator to be calculated again
                         Console.WriteLine("1 - sum\n2 - subtract\n3 - multiply\n4 - divide\n5 - rest of division\n6 - exponentiation\n0 - exit");
                         Console.Write("\nChoose one of the operations above: ");
                         num = int.Parse(Console.ReadLine());
                         break;
-                    // does the division calcule
+                    // division calcule
                     case 4:
-                        // reading of values
+                        // reading values
                         Console.Write("Enter the first number: ");
                         num1 = int.Parse(Console.ReadLine());
                         Console.Write("Enter the second number: ");
@@ -93,19 +93,19 @@ namespace Calculator {
                         // show the result
                         Console.WriteLine("Result: " + op.Dividing(num1, num2));
 
-                        // console reseting
+                        // console reset
                         Console.Write("\nPress enter: ");
                         Console.ReadLine();
                         Console.Clear();
 
-                        // reading again the calculation operator
+                        // reading of the operator to be calculated again
                         Console.WriteLine("1 - sum\n2 - subtract\n3 - multiply\n4 - divide\n5 - rest of division\n6 - exponentiation\n0 - exit");
                         Console.Write("\nChoose one of the operations above: ");
                         num = int.Parse(Console.ReadLine());
                         break;
-                    // does the rest of division calcule
+                    // rest of division calcule
                     case 5:
-                        // reading of values
+                        // reading values
                         Console.Write("Enter the first number: ");
                         num1 = int.Parse(Console.ReadLine());
                         Console.Write("Enter the second number: ");
@@ -114,19 +114,19 @@ namespace Calculator {
                         // show the result
                         Console.WriteLine("Result: " + op.RestOfDivision(num1, num2));
 
-                        // console reseting
+                        // console reset
                         Console.Write("\nPress enter: ");
                         Console.ReadLine();
                         Console.Clear();
 
-                        // reading again the calculation operator
+                        // reading of the operator to be calculated again
                         Console.WriteLine("1 - sum\n2 - subtract\n3 - multiply\n4 - divide\n5 - rest of division\n6 - exponentiation\n0 - exit");
                         Console.Write("\nChoose one of the operations above: ");
                         num = int.Parse(Console.ReadLine());
                         break;
-                    // does the exponentiation calcule
+                    // exponentiation calcule
                     case 6:
-                        // reading of values
+                        // reading values
                         Console.Write("Enter the first number: ");
                         num1 = int.Parse(Console.ReadLine());
                         Console.Write("Enter the second number: ");
@@ -135,12 +135,12 @@ namespace Calculator {
                         // show the result
                         Console.WriteLine("Result: " + op.Exponentiation(num1, num2));
 
-                        // console reseting
+                        // console reset
                         Console.Write("\nPress enter: ");
                         Console.ReadLine();
                         Console.Clear();
 
-                        // reading again the calculation operator
+                        // reading of the operator to be calculated again
                         Console.WriteLine("1 - sum\n2 - subtract\n3 - multiply\n4 - divide\n5 - rest of division\n6 - exponentiation\n0 - exit");
                         Console.Write("\nChoose one of the operations above: ");
                         num = int.Parse(Console.ReadLine());
@@ -150,10 +150,8 @@ namespace Calculator {
                         // request the correct operation
                         Console.Write("\nInvalid number typed! Try again: ");
                         num = int.Parse(Console.ReadLine());
-                        Console.WriteLine();
                         break;
                 }
-            
             }
         }
     }
